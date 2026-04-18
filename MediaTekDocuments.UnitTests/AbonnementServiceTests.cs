e88043ab4b779741ee5b2bdf1b2dff5c037bffb0
Patrick <186724585+patrickbrouhard@users.noreply.github.com>
2026-04-18 11:37:24 +0200

    test: ajout de test unitaire sur la méthode ParutionDansAbonnement

---
+++ MediaTekDocuments.UnitTests/AbonnementServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using MediaTekDocuments.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Microsoft.ApplicationInsights.MetricDimensionNames.TelemetryContext;
+
+namespace MediaTekDocuments.UnitTests
+{
+    [TestClass]
+    public class AbonnementServiceTests
+    {
+        private AbonnementService service;
+
+        [TestInitialize]
+        public void Init()
+        {
+            service = new AbonnementService();
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateDansIntervalle_RetourneTrue()
+        {
+            var result = service.ParutionDansAbonnement(
+                new DateTime(2024, 1, 1),
+                new DateTime(2024, 12, 31),
+                new DateTime(2024, 6, 1) // date de parution dans l'intervalle
+            );
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateSurUneBorne_RetourneTrue()
+        {
+            var result = service.ParutionDansAbonnement(
+                new DateTime(2024, 1, 1),
+                new DateTime(2024, 12, 31),
+                new DateTime(2024, 1, 1) // date de parution égale à la date de commande
+            );
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateAvantCommande_RetourneFalse()
+        {
+            var result = service.ParutionDansAbonnement(
+                new DateTime(2024, 1, 1),
+                new DateTime(2024, 12, 31),
+                new DateTime(2023, 12, 31) // date de parution avant la date de commande
+            );
+
+            Assert.IsFalse(result);
+        }
+
+        [TestMethod]
+        public void ParutionDansAbonnement_DateApresFin_RetourneFalse()
+        {
+            var result = service.ParutionDansAbonnement(
+                new DateTime(2024, 1, 1),
+                new DateTime(2024, 12, 31),
+                new DateTime(2025, 1, 1) // date de parution après la date de fin
+            );
+
+            Assert.IsFalse(result);
+        }
+    }
+}
--- MediaTekDocuments/Services/AbonnementService.cs
+++ MediaTekDocuments/Services/AbonnementService.cs
@@ -7,8 +7,19 @@ using System.Threading.Tasks;
 
 namespace MediaTekDocuments.Services
 {
+    /// <summary>
+    /// Permet de valider si certaines actions (comme la suppression) sont autorisées
+    /// en vérifiant les périodes et les exemplaires associés.
+    /// </summary>
     public class AbonnementService
     {
+        /// <summary>
+        /// Vérifie si une date de parution est comprise dans la période d'un abonnement.
+        /// </summary>
+        /// <param name="dateCommande">Date de début (ou date de commande) de l'abonnement.</param>
+        /// <param name="dateFin">Date de fin de l'abonnement.</param>
+        /// <param name="dateParution">Date de parution à vérifier.</param>
+        /// <returns><c>true</c> si la date de parution se situe entre la date de commande et la date de fin (incluses) ; sinon, <c>false</c>.</returns>
         public bool ParutionDansAbonnement(
             DateTime dateCommande,
             DateTime dateFin,
@@ -19,6 +30,12 @@ namespace MediaTekDocuments.Services
                 && dateParution <= dateFin;
         }
 
+        /// <summary>
+        /// Détermine si un abonnement peut être supprimé en vérifiant qu'aucun exemplaire n'a été rattaché durant sa période de validité.
+        /// </summary>
+        /// <param name="abonnement">L'abonnement que l'on souhaite vérifier.</param>
+        /// <param name="exemplaires">La liste des exemplaires associés de la revue concernée.</param>
+        /// <returns>true si l'abonnement peut être supprimé (aucun exemplaire dans la période) ; sinon, false.</returns>
         public bool PeutSupprimerAbonnement(Abonnement abonnement, List<Exemplaire> exemplaires)
         {
             foreach (var ex in exemplaires)
